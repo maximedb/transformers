846016a14f4625d163bdc5636e8f61e20029cfa5
ydshieh <ydshieh@users.noreply.github.com>
2023-01-16 20:18:40 +0100

    try split CircleCI

--- .circleci/create_circleci_config.py
+++ .circleci/create_circleci_config.py
@@ -110,11 +110,11 @@ class CircleCIJob:
         # test_command = 'TEST=$(echo "tests/models/bert/test_modeling_bert.py tests/models/gpt2/test_modeling_gpt2.py" | circleci tests split) && echo $TEST'
         test_command = 'TEST=$(circleci tests glob "tests/models/bert/*.py" | circleci tests split) && echo $TEST'
         # test_command = 'TEST=$(echo tests/models/bert/__init__.py tests/models/bert/test_modeling_bert.py tests/models/bert/test_modeling_flax_bert.py tests/models/bert/test_modeling_tf_bert.py tests/models/bert/test_tokenization_bert.py tests/models/bert/test_tokenization_bert_tf.py | circleci tests split) && echo $TEST'
-        test_command = 'TEST=$(circleci tests glob "tests/models/bert/*.py") && echo $TEST > circleci_test_files.txt'
+        test_command = 'TEST=$(circleci tests glob "tests/models/bert/*.py") && echo $TEST > circleci_test_files.txt && TESTFILES=$(circleci tests split circleci_test_files.txt) && echo $TESTFILES > circleci_test_files_splitted.txt'
 
         steps.append({"run": {"name": "Run tests", "command": test_command}})
         steps.append({"store_artifacts": {"path": "~/transformers/circleci_test_files.txt"}})
-        steps.append({"store_artifacts": {"path": "~/transformers/reports"}})
+        steps.append({"store_artifacts": {"path": "~/transformers/circleci_test_files_splitted.txt"}})
         job["steps"] = steps
         return job
 
